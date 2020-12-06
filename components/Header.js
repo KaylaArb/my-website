77c9b65c29167abfe85642fad5d015898b0a6796
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2020-12-06 22:59:24 +0100

    icons, header

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Header.module.css'
+
+export default function Header() {
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__title}>
+                <p className={styles.title}>Welcome!</p>
+            </div>
+            <div className={styles.header__navLinks}>
+                <Link href='/https://github.com/KaylaArb'>
+                <img src="/iconmonstr-github-1.svg" alt="gitHub" className={styles.icon}/>
+                </Link>
+                <Link href='/https://www.linkedin.com/in/kayla-arbez-3077a6118/'>
+                <img src="/iconmonstr-linkedin-3.svg" alt="linkedIn" className={styles.icon}/>
+                </Link>
+                <Link href='/resume'>
+                    <a className={styles.text}>
+                        Resume
+                </a>
+                </Link>
+                <Link href='/resume'>
+                    <a className={styles.text}>
+                        Contact
+                </a>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+
+
---
+++ content/project2.md
@@ -0,0 +1,6 @@
+---
+title: "Helllo"
+date: "2020-10-01"
+---
+
+We're building an awesome Next.js blog using Markdown.
