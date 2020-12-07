c59a4a07cebfcd9911164966595a6eaf4f83a528
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2020-12-07 19:32:16 +0100

    bottom contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Link from 'next/link'
 import styles from '../styles/Contact.module.css'
 
 export default function Contact() {
 
   return (
-      <div>
-          
+    <div className={styles.container}>
+      <div className={styles.contact}>
+        <h2 className={styles.contact__title}>Get In Touch</h2>
+        <p className={styles.contact__para}>Currently I am searching for a full time position as an intern/ junior software developer, specializing in either full-stack or Java. Please feel free to get intouch with me via email for any questions or just to say hi!.</p>
+        <Link href='mailto:kaarbez@gmail.com'><button className={styles.contact__button}>Say Hi!</button></Link>
       </div>
-    
+
+    </div>
+
   )
 }
 
