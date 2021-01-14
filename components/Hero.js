c656240ad5e2a2610356a4da6e15988e416ef1fc
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-01-14 23:55:44 +0100

    window load event

--- components/Hero.js
+++ components/Hero.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Link from 'next/link'
 import styles from '../styles/Hero.module.css';
+import {useState, useEffect} from 'react';
 
 export default function Hero() {
+    const [loaded, setloaded] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('load', setloaded(true));
+    })
 
     return (
         <div className={styles.hero}>
             <div className={styles.hero__photoContainer}>
-                <div className={styles.cf}>
-                    <img src="/img4.webp" className={styles.img}/>
-                    <img src="/img3.webp"  className={styles.img}/>
-                    <img src="/img2.webp"  className={styles.img} />
-                    <img src="/img1.webp"  className={styles.img} />
+                <div className={styles.cf} >
+                    <img src="/img4.webp" className={`${styles.img} ${loaded ? styles.active : ''}`}/>
+                    <img src="/img3.webp"  className={`${styles.img} ${loaded ? styles.active : ''}`}/>
+                    <img src="/img2.webp"  className={`${styles.img} ${loaded ? styles.active : ''}`}/>
+                    <img src="/img1.webp"  className={`${styles.img} ${loaded ? styles.active : ''}`}/>
                 </div>
             </div>
             <div className={styles.hero__description}>
