d33d10560d1c1dd191b8a9dcfdd5a415e48ed712
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-01-21 02:43:39 +0100

    spelling

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -10,7 +10,7 @@ export default function AboutMe() {
             <div className={styles.about__container}>
                 <div className={styles.bio}>
                     <div className={styles.decsription}>
-                        <p>Residing in Vancouver BC, I am currently completing my bachelor’s in business administration while self-teaching my-self web and back end development. My background in business and fashion design has tremendously helped in rounding out my skills professionally and in web development.</p>
+                        <p>Residing in Vancouver BC, I am currently completing my bachelor’s in business administration while self-teaching web and back end development. My background in business and fashion design has tremendously helped in rounding out my skills professionally and in web development.</p>
                         <p>In my spare time I enjoy casual weight lifting, watching anime, playing the odd video game (just finished Divinity Sin 2) and traveling (pre-COVID-19 of course).</p>
                         <p>Interesting fact about myself, my last name spelled backwards is ZEBRA.</p>
                     </div>
